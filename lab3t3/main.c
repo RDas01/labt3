cd6ef18115b52294c11ace7f7c503aabd2ddcf80
RDas01 <35583050+RDas01@users.noreply.github.com>
2020-03-12 08:53:24 +1100

    Add files via upload
    task5

--- lab3t3/main.c
+++ lab3t3/main.c
@@ -3,19 +3,25 @@
 
 int main()
 {
-    int n=0, x1, x2;
+    float n=0, x1, x2;
     float xn, xnm1, k, xnpxnm1 = 0;
-    scanf("%d %d", &x1, &x2);
+    scanf("%f %f", &x1, &x2);
     xn = x2;
     xnm1 = x1;
     xnpxnm1 = x1;
-    while(n<10){
+    while(n<100){
         k = xn / xnm1;
         printf("%f\n", xn);
         xnpxnm1 = xnpxnm1 + xn; // sum of all x0,x1,x2...xn
         xnm1 = xn;
         xn = k * xnm1;
         n++;
+    }
+    if(xnm1>xn){
+        printf("converges\n");
+    }
+    else{
+        printf("Diverges\n");
     }
     printf("sum of all values: %f", xnpxnm1);
     return 0;
